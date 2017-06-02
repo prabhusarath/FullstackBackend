a58636f35b0c4a93f1b016a7bcfdc031e6e26fa3
Sarath Kumar Prabhakaran <sprabha7@hawk.iit.edu>
2017-06-02 19:00:41 +0000

    Application Build

--- TravellersStop/index.js
+++ TravellersStop/index.js
@@ -1,7 +1,15 @@
 var express = require("express");
 var app = express();
+var bodyparser = require("body-parser");
 
 app.set("view engine","ejs");
+app.use(bodyparser.urlencoded({extended: true}));
+
+var places = [
+      {name: "3145",image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+      {name: "3245",image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
+      {name: "3345",image:"https://farm9.staticflickr.com/8161/7360193870_cc7945dfea.jpg"}
+      ];
 
 app.get("/",function(req,res){
   res.render("home");
@@ -9,14 +17,22 @@ app.get("/",function(req,res){
 
 
 app.get("/places",function(req,res){
-  
-  var places = [
-      {name: "3145",image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-      {name: "3245",image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-      {name: "3345",image:"https://farm9.staticflickr.com/8161/7360193870_cc7945dfea.jpg"}
-      ];
-  
   res.render("places",{ResPlaces: places});
+});
+
+
+app.post("/places",function(req,res){
+    var namevalues = req.body.dest;
+    var imgvalues = req.body.imgsrc;
+    var newobj = {name: namevalues,image:imgvalues}
+    
+     places.push(newobj);
+     res.redirect("places");
+
+});
+
+app.get("/places/new",function(req,res){
+  res.render("newplaces");
 });
 
 app.listen(process.env.PORT,process.env.IP,function(){
