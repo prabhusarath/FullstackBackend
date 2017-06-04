8baf473c93d98ce280a026b9d29195167597318d
Sarath Kumar Prabhakaran <sprabha7@hawk.iit.edu>
2017-06-04 04:20:08 +0000

    Application Build

--- mongoose/demo.js
+++ mongoose/demo.js
@@ -6,3 +6,20 @@ var schm = new mong.Schema({
    name:String,
    Phone:Number
 });
+
+var dem = mong.model("dogs",schm);
+
+var jith = new dem({
+    name:"Saarth",
+    Phone: 12345
+});
+
+jith.save(function(err,aa){
+   if(err){
+       console.log("Wrong")
+   }
+   else{
+       console.log("Okay");
+       console.log(aa);
+   }
+  });
