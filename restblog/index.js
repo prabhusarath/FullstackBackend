171d4248842a473ab9df21716faaf1e6f31eece2
Sarath Kumar Prabhakaran <sprabha7@hawk.iit.edu>
2017-06-08 01:05:15 +0000

    Blog SemanticUI

--- restblog/index.js
+++ restblog/index.js
@@ -10,14 +10,44 @@ exapp.set("view engine","ejs");
 exapp.use(express.static("public"));
 exapp.use(bodyParser.urlencoded({extended: true}));
 
-var blogs = new mongoose.schema({
+var blogs = new mongoose.Schema({
    title: String,
    image: String,
    body: String,
    creation: {type: Date, default: Date.now}
 });
 
-var blogvar = mongoose.model("blogitems",blogs);
+var blogvar = mongoose.model("blogitem",blogs);
+
+exapp.get("/",function(req,res){
+    res.render("blogs");
+    
+});
+
+exapp.get("/blogs",function(req,res){
+    //res.render("blogs");
+    blogvar.find({},function(err,data){
+        if(err){
+            console.log("Err");
+        }else{
+            res.render("blogs",{blogdata:data})
+        }
+    })
+    
+});
+
+
+// exapp.post("/blogs",function(req,res){
+
+//     });
+
+// exapp.get("/places/new",function(req,res){
+//   res.render("newplaces");
+// });
+
+// exapp.get("/places/:id",function(req,res){
+    
+// });
 
 exapp.listen(process.env.PORT,process.env.IP,function(){
    console.log("Travellers Stop has Started Successfully!!");
